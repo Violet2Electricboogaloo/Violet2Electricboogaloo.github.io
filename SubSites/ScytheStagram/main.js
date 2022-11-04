23c09040cce9312864769ba1bfd643d08f9b9d95
Violets-puragtory <110205356+Violets-puragtory@users.noreply.github.com>
2022-11-04 18:47:12 -0500

    Add files via upload

---
+++ SubSites/ScytheStagram/main.js
@@ -0,0 +1,22 @@
+function test() {
+    $(".mainpage").hide("slow")
+    $(".suggest").hide("slow")
+    $("hr").hide("slow")
+    $("#ScytheDesc").show("slow")
+};
+
+function tes() {
+    $("#CitraDesc").hide("slow")
+    $("#ScytheDesc").hide("slow")
+    $(".mainpage").show("slow")
+    $(".suggest").show("slow")
+    $("hr").show("slow")
+};
+
+function Citra() {
+    $("#ScytheDesc").hide("slow")
+    $(".mainpage").hide("slow")
+    $(".suggest").hide("slow")
+    $("hr").hide("slow")
+    $("#CitraDesc").show("slow")
+};
