89afaa36893a9980a4f63d235b722526546d7075
Violet2Electricboogaloo <111010751+Violet2Electricboogaloo@users.noreply.github.com>
2022-11-18 00:04:51 +0000

    asd

--- SubSites/ScytheStagram/newscythe/main.js
+++ SubSites/ScytheStagram/newscythe/main.js
@@ -1,17 +1,38 @@
 function display(tohide, toshow) {
-    $(toshow).show()
-    $(toshow).css("opacity", "0")
-    $(toshow).transition({
-        "transform": "scaleY(1) scaleX(1)",
-        "opacity": "1"
-    }, 1500, 'cubic-bezier(0.445, 0.05, 0.55, 0.95)')
+
+    setTimeout(() => {
+        $(toshow).show()
+        $(toshow).css("opacity", "0")
+        $(toshow).transition({
+            "opacity": "1"
+        }, 1500, 'cubic-bezier(0.445, 0.05, 0.55, 0.95)')
+    }, 750);
+
 
     $(tohide).transition({
-        "opacity": "0",
-        "transform": "scaleY(0) scaleX(2)"
+        "opacity": "0"
     }, 1500, 'cubic-bezier(0.075, 0.82, 0.165, 1)')
     setTimeout(() => {
-        $(tohide).hide()
+        $(tohide).hide(1500)
     }, 1500);
 }
 
+function mainpage() {
+    setTimeout(() => {
+        const toshow = "#Profiles"
+        $(toshow).show(1500)
+        $(toshow).css("opacity", "0")
+        $(toshow).transition({
+            "opacity": "1"
+        }, 1500, 'cubic-bezier(0.445, 0.05, 0.55, 0.95)')
+    }, 750);
+
+    const tohide = "#descs > *"
+
+    $(tohide).transition({
+        "opacity": "0"
+    }, 1500, 'cubic-bezier(0.075, 0.82, 0.165, 1)')
+    setTimeout(() => {
+        $(tohide).hide(1500)
+    }, 1500);
+}
