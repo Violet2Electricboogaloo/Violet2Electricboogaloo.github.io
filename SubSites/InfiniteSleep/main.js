bd1b61057d1b623d919116da5d646103cf4cd413
Violet2Electricboogaloo <111010751+Violet2Electricboogaloo@users.noreply.github.com>
2022-11-28 16:32:49 +0000

    Sleepy tiem

--- SubSites/InfiniteSleep/main.js
+++ SubSites/InfiniteSleep/main.js
@@ -1,16 +1,40 @@
+var type = "spamm"
+function gensleep() {
+    if (type == "spammy") {
+        $("p").text($("p").text() + " Sleeping")
+        $("button").text("Line Breaks: Off")
+    } else {
+        var sleeper = $("<p></p>").text("Sleeping");
+        $("body").append(sleeper)
+        $("button").text("Line Breaks: On")
+    }
+}
+
 
 $(window).scroll(function() {
     if($(window).scrollTop() + $(window).height() + 1500 >= $(document).height()) {
         for (let index = 0; index < 100; index++) {
-            var sleeper = $("<p></p>").text("Sleeping");
-            $("body").append(sleeper)
+            gensleep()
         }
     }
 });
 
 setTimeout(() => {
     for (let index = 0; index < 100; index++) {
-        var sleeper = $("<p></p>").text("Sleeping");
-        $("body").append(sleeper)
+        gensleep()
     }
 }, 1000);
+
+function breakers() {
+    if(type == "spammy") {
+        type = "spamm"
+    } else {
+        type = "spammy"
+    }
+
+    $("p:not(#infinite)").remove()
+    $("#infinite").text("Sleeping")
+    for (let index = 0; index < 100; index++) {
+        gensleep()
+    }
+}
