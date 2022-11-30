1d40bf8b48cd0dc2f95a0418d1def6147495d979
Violet2Electricboogaloo <111010751+Violet2Electricboogaloo@users.noreply.github.com>
2022-11-30 16:00:28 +0000

    quiz

--- SubSites/HTML Quiz/main.js
+++ SubSites/HTML Quiz/main.js
@@ -1,3 +1,11 @@
+let question = "none"
+
+let partsright = 0
+
+let quesnumb = 1
+
+let answered = false
+
 function lolno() {  
     $("#nobut").transition({"transform": "scale(0)"}, 500, "cubic-bezier(0.55, 0.055, 0.675, 0.19)")
     $("#readyquestion").transition({"color": "red"}, 1000, "cubic-bezier(0.445, 0.05, 0.55, 0.95)")
@@ -21,6 +29,74 @@ function start() {
             "opacity": "0"
         }, 1500, "cubic-bezier(0.445, 0.05, 0.55, 0.95)")
     }, 1500);
+    question = "question1"
+    setTimeout(() => {
+        $("#question1").css("display", "flexbox")
+        $("#question1").show()
+    }, 2500);
+}
 
-    $("#question1").css("display", "flexbox")
+function proceed() {
+    quesnumb++
+
+    $("#" + question).transition({"opacity": "0"}, 7500, "cubic-bezier(0.6, 0.04, 0.98, 0.335)")
+    $("#" + question).hide(5000)
+
+    question = "question" + quesnumb
+
+    setTimeout(() => {
+        $("#" + question).css("display", "flexbox")
+        $("#" + question).css("opacity", "0")
+        $("#" + question).show()
+        $("#" + question).transition({"opacity": "1"}, 1000)
+    }, 100);
+}
+
+function wrong() {
+    $("#" + question + "> h1").text("Wrong!")
+    $("#" + question + "> .answer").show(1000)
+    $("#" + question + "> .answer").css("display", "flexbox")
+    $("#" + question + "> .wrong").hide(1000)
+    $("#" + question + "> button").prop("disabled", "true")
+
+    answered = true
+
+    proceed()
+}
+
+function right() {
+    $("#" + question + "> h1").text("Correct!")
+    $("#" + question + "> .wrong").hide(1000)
+    $("#" + question + "> button").prop("disabled", "true")
+
+    answered = true
+
+    proceed()
+}
+
+function wrongpart() {
+    $("#" + question + "> h1").text("Wrong!")
+    $("#" + question + "> .answer").show(1000)
+    $("#" + question + "> .answer").css("display", "flexbox")
+    $("#" + question + "> .wrong").hide(1000)
+    $("#" + question + "> button").prop("disabled", "true")
+
+    answered = true
+}
+
+function rightpart(wha) {
+    //$("#" + question + "> h1").text("Correct!")
+    //$("#" + question + "> .wrong").hide(1000)
+    //$("#" + question + "> button").prop("disabled", "true")
+
+    partsright++
+
+    $(".the" + wha).css("background", "green")
+    $(".the" + wha).css("color", "white")
+
+    if(partsright === 3) {
+        partsright = 0
+        right()
+        proceed()
+    }
 }
