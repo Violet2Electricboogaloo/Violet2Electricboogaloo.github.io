e0db554f1258db265c816fdab6ace5275f27b335
Violet2Electricboogaloo <111010751+Violet2Electricboogaloo@users.noreply.github.com>
2022-12-06 23:49:13 +0000

    A

--- SubSites/ScytheTicTacToe/main.js
+++ SubSites/ScytheTicTacToe/main.js
@@ -1,24 +1,90 @@
+let biggered = false
+
 function tester(which) {
-    $("#" + which).css("overflow", "visible")
-    
-    $("#" + which + "> .minicontent").transition({
-        "transform": " translate(0px, -19vh)",
-        "background-color": "rgba(35, 15, 50)",
-        "overflow-y": "scroll",
-        "min-height": "100vh",
-        "width": "100vw"
-    }, 1500, "cubic-bezier(0.39, 0.575, 0.565, 1)")
-    
-    $(`.biggiecheese > *:not(#${which}), body > h1`).transition({
-        "opacity": "0"
-    }, 1500)
+    if (biggered == false) {
+        $("#" + which).css("overflow", "visible")
 
-    $("body, html").transition({
-        "background-color": "rgba(35, 15, 50)"
-    }, 1500, "cubic-bezier(0.39, 0.575, 0.565, 1)")
+        let horizontal = "0vw"
 
-    setTimeout(() => {
-        $(".biggiecheese > *, body > h1").css("visibility", "hidden")
-        $("#" + which + "> .minicontent").css("visibility", "visible")
-    }, 1500);
+        let vertical = "-14vh"
+
+
+        $("#" + which).css("z-index", "150")
+
+
+        $("p, h1").text()
+
+        if(which.charAt(3) == "2") {
+            vertical = "-45vh"
+        } else if (which.charAt(3) == "3") {
+            vertical = "-75vh"
+        }
+
+        if(which.slice(-1) == "2") {
+            horizontal = "-35vw"
+        } else if(which.slice(-1) == "3") {
+            horizontal = "-70vw"
+        }
+
+        $("#" + which + "> .minicontent").transition({
+            "transform": `translate(${horizontal}, ${vertical})`,
+            "background-color": "rgba(35, 15, 50, 100)",
+            "overflow-y": "scroll",
+            "min-height": "100vh",
+            "width": "100vw"
+        }, 1500, "cubic-bezier(0.39, 0.575, 0.565, 1)")
+
+        
+        $(`.biggiecheese > *:not(#${which}), body > h1`).transition({
+            "opacity": "0"
+        }, 1500)
+    
+        $("body, html").transition({
+            "background-color": "rgba(35, 15, 50)"
+        }, 1500, "cubic-bezier(0.39, 0.575, 0.565, 1)")
+    
+        setTimeout(() => {
+        biggered = true
+            $(".biggiecheese > *, body > h1").css("visibility", "hidden")
+            $("#" + which + "> .minicontent").css("visibility", "visible")
+        }, 1500);
+    }
+}
+
+function theecks(which) {
+    if(biggered == true) {
+
+        $("#" + which).css("background-color", "rgba(35, 15, 50")
+
+        $("#" + which).css("z-index", "1")
+    
+        $("#" + which + "> .minicontent").transition({
+            "transform": "scale(0.3) translate(-115%, -120%)",
+            "background-color": "rgba(35, 15, 50, 0)",
+            "overflow-y": "hidden",
+            "min-height": "100vh",
+            "width": "100vw"
+        }, 1500, "cubic-bezier(0.39, 0.575, 0.565, 1)")
+        
+        // $(`.biggiecheese > *:not(#${which}), body > h1`).transition({
+        //     "opacity": "1"
+        // }, 1500)
+
+
+        $(".biggiecheese > *, body > h1").css("visibility", "visible")
+
+        $(`.biggiecheese > *:not(#${which}), body > h1`).css("opacity", "1")
+    
+        $("body, html").transition({
+            "background-color": "rgba(45, 5, 25)"
+        }, 1500, "cubic-bezier(0.39, 0.575, 0.565, 1)")
+    
+        setTimeout(() => {
+        $("#" + which).css("overflow", "hidden")
+            $("#" + which + "> .minicontent").css("visibility", "visible")
+
+        biggered = false
+        }, 1500);
+    }
+
 }
